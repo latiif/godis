7cfc24b8816b92fa5ab7e2f954916f94cd5366a1
Latiif <abdullatif.alshriaf@combitech.se>
2020-04-13 14:00:17 +0200

    Add python script to extract data from raw e-numbers

---
+++ assets/parse.py
@@ -0,0 +1,24 @@
+import json
+
+f = open("data.txt", "r")
+
+result= dict()
+
+for line in f:
+    components = line.split()
+    name = components[0]
+    comment = list()
+    tags  = list()
+
+    for word in components[1:]:
+        if (word.isupper()):
+            tags.append(word)
+        else:
+            comment.append(word)
+
+    result[name] = dict()
+    result[name]["tags"]=tags
+    result[name]["description"]=" ".join(comment)
+
+json_string = json.dumps(result)
+print(json_string)

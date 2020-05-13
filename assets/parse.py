67f6c90dae1cd832d199ba72bd9817ac8d505cbc
latiif <latiif@mail.com>
2020-05-13 19:11:04 +0200

    Re-organize parse.py

--- assets/parse.py
+++ assets/parse.py
@@ -1,24 +1,32 @@
 import json
 
-f = open("./assets/data.txt", "r")
 
-result = dict()
+def parse_data_file(file_name: str) -> dict:
+    f = open(file_name, "r")
 
-for line in f:
-    components = line.split()
-    name = components[0]
-    comment = list()
-    tags = list()
+    result = dict()
+    for line in f:
+        components = line.split()
+        name = components[0]
+        comment = list()
+        tags = list()
+        for word in components[1:]:
+            if (word.isupper()):
+                tags.append(word)
+            else:
+                comment.append(word)
 
-    for word in components[1:]:
-        if (word.isupper()):
-            tags.append(word)
-        else:
-            comment.append(word)
+        result[name] = dict()
+        result[name]["tags"] = tags
+        result[name]["description"] = " ".join(comment)
 
-    result[name] = dict()
-    result[name]["tags"] = tags
-    result[name]["description"] = " ".join(comment)
+    return result
 
-json_string = json.dumps(result)
-print('let data ={}\n\nexport {}'.format(json_string, "{ data }"))
+
+def spew_dict_as_ts(data_dict: dict) -> str:
+    json_string = json.dumps(data_dict)
+    return 'let data ={}\n\nexport {}'.format(json_string, "{ data }")
+
+
+if __name__ == "__main__":
+    print(spew_dict_as_ts(parse_data_file("./assets/data.txt")))
